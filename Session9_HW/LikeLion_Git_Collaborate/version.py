c37e466dd26fe920632020e2ad3b28aee4fface3
KongDagam <rwg0712@naver.com>
2021-05-24 20:04:37 +0900

    session11

---
+++ Session9_HW/LikeLion_Git_Collaborate/version.py
@@ -0,0 +1,20 @@
+# 첫 번째 커밋
+print("첫 번째 커밋입니다.")
+
+# 두 번째 커밋
+<<<<<<< HEAD
+while TRUE:
+    print("hihihi")
+    
+
+# 세 번째 커밋
+a = "충돌충돌충돌"
+print(a + 123)
+=======
+if true :
+    print("conflcit1")
+
+# 세 번째 커밋
+if true :
+    print("conflcit1")
+>>>>>>> feat/conflict1
